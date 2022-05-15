5056219f5693d46bc1bd6ffc0e0ab52781b401f6
melohan <melodie.ohan@cpnv.ch>
2022-05-15 23:47:44 +0200

    Add methods according to unit tests

--- src/igw/Igw.js
+++ src/igw/Igw.js
@@ -10,11 +10,75 @@
 const config = require('../config');
 const IgwcException = require("./IgwException");
 const IgwNotFoundException = require("./IgwNotFoundException");
+const IgwNameNotAvailable = require("../igw/IgwNameNotAvailable");
 
 
 module.exports = class Igw {
 
+
+    //region private attributes
+    #client = null;
+    igwId = null;
+    name = null;
+    //endregion private attributes
+
     constructor() {
+    }
+    
+    /**
+     * @brief This method creates an igw with the given name
+     * @param name : name of the internet gateway 
+     * @exception IgwNameNotAvailable is thrown when the name is already in use
+     */
+    create(name){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * @brief This property gets the internet gateway id
+     */
+    igwId(){
+        throw new Error('Method not implemented.');
+    }    
+
+    /**
+     * @brief This property gets the name of the internet gateway
+     */
+    name(){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * @brief This method checks if the name is available
+     * @param name:  name of the internet gateway   
+     */
+    static nameAvailable(name){
+        throw new Error('Method not implemented.');
+    }
+    
+    /**
+     * @brief This method returns the id of the internet gatway by its name
+     * @param name: name of the internet gateway  to find
+     */
+    static findId(name){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * @brief This method deletes an internet gateway by its name
+     * @param name 
+     * @exception IgwNotFoundException is thrown when attempts to delete non-existent Igw
+     */
+    static deleteOne(name){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * @brief This method get all existing internet gateway.
+     * @returns list of internet gateway
+     */
+    static all(){
+        throw new Error('Method not implemented.');
     }
 
 }
