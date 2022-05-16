33345828e2e1f2e6151d0ba0189bf58aba716355
melohan <melodie.ohan@cpnv.ch>
2022-05-16 23:51:53 +0200

    Update class attribute and constructor, add methods find and delete

--- src/igw/Igw.js
+++ src/igw/Igw.js
@@ -17,25 +17,21 @@ module.exports = class Igw {
 
 
     //region private attributes
-    #client = null;
     igwId = null;
     name = null;
     //endregion private attributes
 
-    constructor() {
+    /**
+     * 
+     * @param name
+     * @exception IgwNameNotAvailable is thrown when name is already used
+     */
+    constructor(name, id = null) {  
     }
     
     /**
-     * @brief This method creates an igw with the given name
-     * @param name : name of the internet gateway 
-     * @exception IgwNameNotAvailable is thrown when the name is already in use
-     */
-    create(name){
-        throw new Error('Method not implemented.');
-    }
-
-    /**
      * @brief This property gets the internet gateway id
+     * @returns igw id
      */
     igwId(){
         throw new Error('Method not implemented.');
@@ -43,24 +39,53 @@ module.exports = class Igw {
 
     /**
      * @brief This property gets the name of the internet gateway
+     * @returns name
      */
     name(){
         throw new Error('Method not implemented.');
     }
 
     /**
+     * @brief This method deletes an internet gateway by its name
+     * @exception IgwNotFoundException is thrown when attempts to delete non-existent Igw
+     */
+    delete(){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
      * @brief This method checks if the name is available
-     * @param name:  name of the internet gateway   
+     * @param name:  name of the internet gateway
+     * @returns true if name is available
      */
     static nameAvailable(name){
         throw new Error('Method not implemented.');
     }
     
     /**
+     * @brief This method returns the the internet gatway object by its name
+     * @param name: name of the internet gateway  to find
+     * @returns null if igw doesn't exist or Igw object
+     */
+    static find(name){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
      * @brief This method returns the id of the internet gatway by its name
      * @param name: name of the internet gateway  to find
      */
     static findId(name){
+        throw new Error('Method not implemented.');
+    }
+
+    /**
+     * @brief This method creates an igw with the given name
+     * @param name : name of the internet gateway 
+     * @returns 
+     * @exception IgwNameNotAvailable is thrown when the name is already in use
+     */
+    static create(name){
         throw new Error('Method not implemented.');
     }
 
