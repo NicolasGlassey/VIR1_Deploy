78552c1240ec1fc9cc01601e279a0420a2d5407e
Nomeos <mathieu.rabot01@gmail.com>
2022-06-14 00:54:49 +0200

    declare some variable needed for the tests

--- src/tests/subnetHelper.test.js
+++ src/tests/subnetHelper.test.js
@@ -6,13 +6,26 @@
  */
 
 const SubnetHelper = require("../subnet/subnetHelper.js");
+const VpcHelper = require("../vpc/VpcHelper");
+const SubnetNameNotAvailableException = require("../subnet/SubnetNameNotAvailableException");
+const SubnetNotFoundException = require("../subnet/SubnetNotFoundException");
 
+let vpcHelper;
 let subnetHelper;
 let subnetName;
+let vpcName;
+let vpcCidr;
+let subnetCidr;
+let availabilityZone;
 
 beforeEach(() => {
+    vpcHelper = new VpcHelper("eu-west-3");
     subnetHelper = new SubnetHelper("eu-west-3");
     subnetName = "Subnet_TEST_Deploy";
+    vpcName = "VPC_TEST_Deploy";
+    vpcCidr = "10.0.0.0/16";
+    subnetCidr = "10.0.0.0/24";
+    availabilityZone = "eu-west-3a";
 })
 
 /**
