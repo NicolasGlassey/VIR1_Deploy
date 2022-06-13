652e995528f62edfbb2be291487275e85ba8c40a
Nomeos <mathieu.rabot01@gmail.com>
2022-06-13 21:08:14 +0200

    Create subnetHelper test file with header and default before/after each functions

---
+++ src/tests/subnetHelper.test.js
@@ -0,0 +1,14 @@
+/**
+ * @file     subnetHelper.test.js
+ * @brief    This class is designed to test the behaviour of a Subnet.
+ * @author   Mathieu Rabot
+ * @version  13-06-2022 - original (dedicated to VIR1)
+ */
+
+beforeEach(() => {
+
+})
+
+afterEach(() => {
+
+})
