c29c5296ce04f01df36eb679c04c8e60807d580d
Nomeos <mathieu.rabot01@gmail.com>
2022-06-13 21:00:46 +0200

    Create subnetHelper file with header

---
+++ src/subnet/subnetHelper.js
@@ -0,0 +1,8 @@
+/**
+ * @file     subnetHelper.js
+ * @brief    This class is designed to manage an aws Subnet
+ * @author   Mathieu Rabot
+ * @version  13-06-2022 - original (dedicated to VIR1)
+ */
+
+module.exports = class SubnetHelper {}
