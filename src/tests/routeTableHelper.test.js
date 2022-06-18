50ede32b96e6fb1a422b91bc3cc41c4e3a891cd8
melohan <melodie.ohan@cpnv.ch>
2022-06-19 00:47:11 +0200

    Update and implement each tests

--- src/tests/routeTableHelper.test.js
+++ src/tests/routeTableHelper.test.js
@@ -13,128 +13,141 @@ const VpcHelper = require('../vpc/VpcHelper.js');
 const VpcNotFoundException = require("../vpc/VpcNotFoundException.js");
 
 const SubnetHelper = require('../subnet/subnetHelper.js');
-const SubnetNotFoundException = require("../subnet/SubnetNotFoundException.js");
 
 const RouteTableHelper = require("../routeTable/RouteTableHelper.js");
 const RouteTableAlreadyExistsException = require("../routeTable/RouteTableAlreadyExistsException.js");
 const RouteTableNotFoundException = require("../routeTable/RouteTableNotFoundException.js");
 
 
-beforeEach(() => {
+  let vpcHelper, routeTableHelper, vpcName, vpcCidr, routeTableName, region, subnetName;
+
+  let subnetHelper, availabilityZone;
+
+beforeAll(() => {
+    region = "eu-west-3";
+    routeTableHelper = new RouteTableHelper(region);
+    vpcHelper = new VpcHelper(region);
+    vpcName = "vpc-deploy-test";
+    vpcCidr = "10.0.0.0/16";
+    routeTableName = "routeTable-deploy-test"
+    subnetName = "subnet-deploy-test";
+    subnetHelper = new SubnetHelper(region);
+    availabilityZone = "eu-west-3a";
+});
+
+beforeEach(async()=>{
+    if(await routeTableHelper.isAssociated(routeTableName) === true) await routeTableHelper.deasassociate(routeTableName, subnetName)
+    if(await vpcHelper.exists(vpcName) === false) await vpcHelper.create(vpcName, vpcCidr);
+    if(await routeTableHelper.exists(routeTableName) === false) await routeTableHelper.create(routeTableName, vpcName);
+})
+
+afterEach(async () => {
+    if(await routeTableHelper.isAssociated(routeTableName) === true) await routeTableHelper.deasassociate(routeTableName, subnetName)
+    if(await subnetHelper.exists(subnetName)) await subnetHelper.delete(subnetName);
+    if(await routeTableHelper.exists(routeTableName) === true) await routeTableHelper.delete(routeTableName);
+    if(await vpcHelper.exists(vpcName) === true) await vpcHelper.delete(vpcName);
 });
 
 
-test('create_NominalCase_Success', async() => {
+test('findId_NominalCase_Success', async() => {
     // given
-    
-    // when
-
-    // then
-})
-
-test('create_VpcDoesNotExist_ThrowException', async() => {
-})
-
-test('associate_NominalCase_Success', async() => {
-    // given
-    
-    // when
-
-    // then
-})
-
-test('associate_VpcNotFound_ThrowException', async() => {
-    // given
+    // refer to beforeAll method
 
     // when
+    // refer to beforeEach method
 
     // then
+    expect(await routeTableHelper.findId(routeTableName)).not.toEqual(null)
 })
 
-test('deasassociate_NominalCase_Success', async() => {
+test('findId_NonExistentRouteTable_Success', async() => {
     // given
+    let notUsedName = "NonExistentRouteTable";
 
     // when
+    // nothing is created
 
     // then
-})
-
-test('deasassociate_VpcNotFound_ThrowException', async() => {
-    // given
-
-    // when
-
-    // then
-})
-
-test('state_Associated_Success', async() => {
-    // given
-
-    // when
-
-    // then
-})
-
-test('state_Deasassociated_Success', async() => {
-    // given
-
-    // when
-
-    // then
-})
-
-test('delete_Deasassociated_Success', async() => {
-    // given
-
-    // when
-
-    // then
-})
-
-test('delete_Associated_ThrowException', async() => {
-    // given
-
-    // when
-
-    // then
+    expect(await routeTableHelper.findId(notUsedName)).toEqual(null);
 })
 
 test('exists_NominalCase_Success', async() => {
     // given
+    // refer to beforeAll method
 
     // when
+    // refer to beforeEach method
 
     // then
+    expect(await routeTableHelper.exists(routeTableName)).toEqual(true)
 })
 
-test('exists_NotTableRoute_Success', async() => {
+test('findId_NonExistentRouteTable_Success', async() => {
     // given
+    let notUsedName = "NonExistentRouteTable";
 
     // when
+    // nothing is created
 
     // then
+    expect(await routeTableHelper.exists(notUsedName)).toEqual(false);
 })
 
-test('findId_TableRouteExists_Success', async() => {
+test('create_NominalCase_Success', async() => {
     // given
+    // refer to beforeAll method
 
     // when
+    // refer to beforeEach method
 
     // then
+    expect(await routeTableHelper.exists(routeTableName)).toEqual(true)
 })
 
-test('findId_TableRouteDoesNotExist_Success', async() => {
+  
+test('create_VpcDoesNotExist_ThrowException', async() => {
     // given
-
+    let nonExistentVpc = "non-existent";
+    
     // when
+    await expect(routeTableHelper.create(routeTableName, nonExistentVpc)).rejects.toThrow(VpcNotFoundException);
 
     // then
+    // Exception is thrown
+})
+  
+test('delete_Deasassociated_Success', async() => {
+    // given
+    // refer to beforeEach and beforeAll methods
+
+    // when
+    await routeTableHelper.delete(routeTableName);
+
+    // then
+    expect(await routeTableHelper.exists(routeTableName)).toEqual(false)
 })
 
-test('all_TableRoutesExist_Success', async() => {
+test('isAssociated_RouteTableNotAssociated_Success', async() => {
     // given
+    // refer to beforeEach and beforeAll methods
 
     // when
+    let isAssociated = routeTableHelper.isAssociated(routeTableName);
 
     // then
+    expect(await routeTableHelper.isAssociated(routeTableName)).toEqual(false);
+})
+
+test('isAssociated_RouteTableAssociated_Success', async() => {
+    // given
+    // refer to beforeAll method
+  
+    // refer to beforeEach method
+    await subnetHelper.create(subnetName, vpcName, vpcCidr, availabilityZone);
+
+    // when
+    await routeTableHelper.associate(routeTableName, subnetName);
+
+    // then
+    expect(await routeTableHelper.isAssociated(routeTableName)).toEqual(true);
 })
